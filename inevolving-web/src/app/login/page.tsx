0326cac93c7c8e11079664f9684f32574473b49b
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-07-03 22:27:30 -0300

    Finalizando Pagina de Input de Login

--- inevolving-web/src/app/login/page.tsx
+++ inevolving-web/src/app/login/page.tsx
@@ -1,10 +1,21 @@
+'use client';
+
 import CardTextoImagem from "@/components/CardTextoImagem";
 import styles from "./page.module.scss";
 import CardInputLogin from "@/components/CardInputLogin";
+import { motion } from "motion/react";
 
 export default function Login() {
     return (
-        <div className={styles.background}>
+        <motion.div 
+            className={styles.background}
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.4,
+                scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+            }}
+        >
             <div>
                 <CardTextoImagem texto={
                         <>
@@ -15,6 +26,6 @@ export default function Login() {
                 />
             </div>
             <CardInputLogin />
-        </div>
+        </motion.div>
     );
 }
