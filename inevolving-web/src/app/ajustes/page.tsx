f842b3e3a371aabd11f766bcc84f48a9a3bbf56b
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-27 14:59:33 -0300

    Refatorando página de ajustes

--- inevolving-web/src/app/ajustes/page.tsx
+++ inevolving-web/src/app/ajustes/page.tsx
@@ -2,20 +2,16 @@
 
 export const dynamic = "force-dynamic";
 
-// import { useEffect, useState } from 'react';
 import Menu from "@/components/Menu";
 import styles from './page.module.scss';
-// import Image from "next/image";
 import * as motion from "motion/react-client";
 import { useEffect, useState } from "react";
-// import { ClipLoader } from 'react-spinners';
-// import { ResponseDashboard } from '@/components/interfaces/ResponseDashboard';
 
-export default function Categoria( ) {
+export default function Page( ) {
+
     const [isMobile, setIsMobile] = useState(false);
     const [tipoMenuDesk, setTipoMenuDesk] = useState<number | undefined>(undefined);
 
-    
     useEffect(() => {
         if (typeof window !== 'undefined') {
 
@@ -29,74 +25,34 @@ export default function Categoria( ) {
         }
     }, []);
 
-
-
-
-    if (!isMobile) {
-        if (tipoMenuDesk === 1) {
-            return (
-                <motion.div>
-                    <Menu />
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            duration: 0.4,
-                            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
-                        }} 
-                        className={styles.container}
+    return (
+        <motion.div className={isMobile ? styles.mob : tipoMenuDesk === 2 ? styles.containerTipoMenu2 : ''}>
+            <Menu />
+            <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.4,
+                    scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+                }} 
+                className={styles.container}
+            >
+                <div className={styles.tituloContainer}>
+                    <h1>Ajustes</h1>
+                    <p>Altere suas informações de login</p>
+                </div>
+                <motion.div className={styles.containerConteudo}>
+                    <button 
+                        onClick={() => {
+                            const novoTipo = tipoMenuDesk === 1 ? 2 : 1;
+                            localStorage.setItem('tipoMenuDesk', novoTipo.toString());
+                            window.location.reload();
+                        }}
                     >
-                        <div className={styles.tituloContainer}>
-                            <h1>Ajustes</h1>
-                            <p>Altere suas informações de login</p>
-                        </div>
-                        <motion.div className={styles.containerConteudo}>
-                            <button 
-                                onClick={() => {
-                                    const novoTipo = tipoMenuDesk === 1 ? 2 : 1;
-                                    localStorage.setItem('tipoMenuDesk', novoTipo.toString());
-                                    window.location.reload();
-                                }}
-                            >
-                                alterar tipo do menu!
-                            </button>
-                        </motion.div>
-                    </motion.div>
+                        alterar tipo do menu!
+                    </button>
                 </motion.div>
-            );
-        } else if (tipoMenuDesk === 2) {
-            return (
-                <motion.div className={styles.containerTipoMenu2}>
-                    <Menu />
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            duration: 0.4,
-                            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
-                        }} 
-                        className={styles.container}
-                    >
-                        <div className={styles.tituloContainer}>
-                            <h1>Ajustes</h1>
-                            <p>Altere suas informações de login</p>
-                        </div>
-                        <motion.div className={styles.containerConteudo}>
-                            <button 
-                                onClick={() => {
-                                    const novoTipo = tipoMenuDesk === 2 ? 1 : 2;
-                                    localStorage.setItem('tipoMenuDesk', novoTipo.toString());
-                                    window.location.reload();
-                                }}
-                            >
-                                alterar tipo do menu!
-                            </button>
-                        </motion.div>
-                    </motion.div>
-                </motion.div>
-            );
-        }
-    } else {
-        
-    }
+            </motion.div>
+        </motion.div>
+    );
 }
