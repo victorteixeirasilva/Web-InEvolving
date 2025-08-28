ad9264f2277e1d2178506c1702c5e0140164a5d1
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-28 18:59:16 -0300

    Refatorando Componente CardInputCadastro

--- inevolving-web/src/components/CardInputCadastro/index.tsx
+++ inevolving-web/src/components/CardInputCadastro/index.tsx
@@ -7,6 +7,7 @@ import InputEmail from '../InputEmail';
 import { useRouter } from 'next/navigation';
 import { motion } from "motion/react";
 import { ClipLoader } from 'react-spinners';
+import { linkApi } from '@/app/page';
 
 
 export default function CardInputCadastro({ preEmail }: { preEmail?: string }) {
@@ -21,18 +22,22 @@ export default function CardInputCadastro({ preEmail }: { preEmail?: string }) {
     const [email, setEmail] = useState(preEmail || '');
     const [senha, setSenha] = useState('');
     const [confirmarSenha, setConfirmarSenha] = useState('');
-    const [telefone, setTelefone] = useState('');
 
     const router = useRouter();
 
     const handleCadastro = async () => {
+        
         setCarregando(true);
+        
         if (senha !== confirmarSenha) {
             alert('As senhas não coincidem');
             setCarregando(false);
             return;
         }
-        const response = await fetch('https://api.inevolving.inovasoft.tech/api/authentication/register', {
+        
+        const response = await fetch(
+           linkApi + '/api/authentication/register', 
+        {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json',
@@ -59,110 +64,67 @@ export default function CardInputCadastro({ preEmail }: { preEmail?: string }) {
 
     };
     
-    if (isMobile) {
-        return (
-            <div className={styles.mob}>
-                    <motion.div 
-                        className={styles.container}
-                        initial={{ opacity: 0, scale: 0.5 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            duration: 0.5,
-                            delay: 0.05,
-                            ease: [0, 0.71, 0.2, 1.01],
-                        }}
-                    >
-                        <h1>
-                            Criar conta
-                        </h1>
-                        
-                        <InputEmail tema="escuro" tipo="email" value={email} onChange={setEmail} />
-                        
-                        <InputEmail tema="escuro" tipo="senha" value={senha} onChange={setSenha} />
-                        
-                        <InputEmail tema="escuro" tipo="senha" value={confirmarSenha} onChange={setConfirmarSenha} />
-                        
-                        {/* <InputEmail tema="escuro" tipo="telefone" value={telefone} onChange={setTelefone} /> */}
-        
-                        <p>
-                            
-                        </p>
-        
+    return (
+        <div className={isMobile ? styles.mob : styles.cardRegistro}>
+            <motion.div 
+                className={styles.container}
+                initial={isMobile ? { opacity: 0, scale: 0.5 } : {}}
+                animate={isMobile ? { opacity: 1, scale: 1 } : {}}
+                transition={isMobile ? {
+                    duration: 0.5,
+                    delay: 0.05,
+                    ease: [0, 0.71, 0.2, 1.01],
+                } : {}}
+            >
+                <h1>
+                    Criar conta
+                </h1>
 
-                        <motion.button 
-                            whileTap={{ scale: 0.8 }}
-                            disabled={carregando} 
-                            type="submit" 
-                            className={styles.botaoGrande} 
-                            onClick={handleCadastro}
-                        >
-                        {carregando && <ClipLoader size={10} color="#ffffff" />}
-                        <span 
-                            style={{ 
-                                marginLeft: carregando ? '8px' : '0'
-                            }}
-                        >
-                            {carregando ? 'Entrando...' : 'Entrar'}
-                        </span>
-                        Concluido
-                    </motion.button>
-                        
-                    </motion.div>
-            </div>
-        );
-    } else {
-        return (
-            <div className={styles.cardRegistro}>
-                <div className={styles.container}>
-                    <h1>
-                        Criar conta
-                    </h1>
+                {!isMobile && (
                     <div className={styles.subtitulo}>
                         <p>       
                             Preencha com seus dados
                         </p>
                         <Image 
-                            src="dados.svg"
+                            src="/dados.svg"
                             alt='Selo de dados'
                             width={11}
                             height={13}
                         />
                     </div>
+                )}
+                
+                <InputEmail tema="escuro" tipo="email" value={email} onChange={setEmail} />
+                
+                <InputEmail tema="escuro" tipo="senha" value={senha} onChange={setSenha} />
+                
+                <InputEmail tema="escuro" tipo="senha" value={confirmarSenha} onChange={setConfirmarSenha} />
+                
+                {/* <InputEmail tema="escuro" tipo="telefone" value={telefone} onChange={setTelefone} /> */}
+
+                <p>
                     
-                    <InputEmail tema="escuro" tipo="email" value={email} onChange={setEmail} />
-                    
-                    <InputEmail tema="escuro" tipo="senha" value={senha} onChange={setSenha} />
-                    
-                    <InputEmail tema="escuro" tipo="senha" value={confirmarSenha} onChange={setConfirmarSenha} />
-                    
-                    <InputEmail tema="escuro" tipo="telefone" value={telefone} onChange={setTelefone} />
-    
-                    <p>
-                        
-                    </p>
-    
-                    {/* <BotaoLogin texto='Concluido' tipo='3' onClick={handleCadastro}/> */}
-                    <motion.button 
-                            whileTap={{ scale: 0.8 }}
-                            disabled={carregando} 
-                            type="submit" 
-                            className={styles.botaoGrande} 
-                            onClick={handleCadastro}
+                </p>
+
+
+                <motion.button 
+                    whileTap={{ scale: 0.8 }}
+                    disabled={carregando} 
+                    type="submit" 
+                    className={styles.botaoGrande} 
+                    onClick={handleCadastro}
+                >
+                    {carregando && <ClipLoader size={10} color="#ffffff" />}
+                    <span 
+                        style={{ 
+                            marginLeft: carregando ? '8px' : '0'
+                        }}
                     >
-                        {carregando && <ClipLoader size={10} color="#ffffff" />}
-                        <span 
-                            style={{ 
-                                marginLeft: carregando ? '8px' : '0'
-                            }}
-                        >
-                            {carregando ? 'Cadastrando...' : 'Cadastrar'}
-                        </span>
-                        {/* Concluido */}
-                    </motion.button>
-                    
-                </div>
-            </div>
-        );
-    }
+                        {carregando ? 'Cadastrando...' : 'Cadastrar'}
+                    </span>
+                </motion.button>
+            </motion.div>
+        </div>
+    );
 
 } 
