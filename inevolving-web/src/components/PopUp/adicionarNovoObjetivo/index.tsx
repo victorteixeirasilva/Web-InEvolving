6a08f56bc97553b82c14eda50728190a7550d678
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-29 15:59:28 -0300

    Refatorando Componente AdicionarNovoObjetivo

--- inevolving-web/src/components/PopUp/adicionarNovoObjetivo/index.tsx
+++ inevolving-web/src/components/PopUp/adicionarNovoObjetivo/index.tsx
@@ -4,6 +4,7 @@ import * as motion from "motion/react-client";
 import { useEffect, useState } from "react";
 import { ClipLoader } from 'react-spinners';
 import { useRouter } from "next/navigation";
+import { linkApi } from "@/app/page";
 
 export default function AdicionarNovoObjetivo() {
     const [isMobile, setIsMobile] = useState(false);
@@ -28,17 +29,20 @@ export default function AdicionarNovoObjetivo() {
     const handleRegistrarObjetivo = async () => {
         setCarregando(true);
 
-        const response = await fetch('https://api.inevolving.inovasoft.tech/auth/api/objectives', {
-            method: 'POST',
-            headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + jwtToken
-            },
-            body: JSON.stringify({
-                nameObjective: nomeObjetivo,
-                descriptionObjective: descricaoObjetivo,
-            }),
-        });
+        const response = await fetch(
+            linkApi + '/auth/api/objectives', 
+            {
+                method: 'POST',
+                headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + jwtToken
+                },
+                body: JSON.stringify({
+                    nameObjective: nomeObjetivo,
+                    descriptionObjective: descricaoObjetivo,
+                }),
+            }
+        );
 
         const data = await response.json();
 
@@ -60,8 +64,8 @@ export default function AdicionarNovoObjetivo() {
         }
     };
 
-    if (!isMobile) {
-        return (
+    return (
+        <div className={isMobile ? styles.mob : ''}>
             <div className={styles.overlay}>
                 <div className={styles.containerPopUp}>
                     <motion.button
@@ -70,7 +74,7 @@ export default function AdicionarNovoObjetivo() {
                         className={styles.botaoVoltar} 
                         onClick={() => window.location.reload()}
                     >
-                        <strong>X</strong>
+                        <strong style={{color: '#0B0E31'}}>X</strong>
                     </motion.button>
                     <div className={styles.conteudo}>
                         <Image 
@@ -144,94 +148,95 @@ export default function AdicionarNovoObjetivo() {
                     </div>
                 </div>
             </div>
-        );
-    } else {
-        return (
-            <div className={styles.mob}>
-                <div className={styles.overlay}>
-                    <div className={styles.containerPopUp}>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }} 
-                            whileTap={{ scale: 0.8 }}
-                            className={styles.botaoVoltar} 
-                            onClick={() => window.location.reload()}
-                        >
-                            <strong style={{color: '#0B0E31'}}>X</strong>
-                        </motion.button>
-                        <div className={styles.conteudo}>
-                            <Image 
-                                src="/iconeObjetivo-NovoObjetivo.svg"
-                                alt="Icone Objetivo"
-                                width={72}
-                                height={72}
-                                className={styles.icone}
-                            />
-                            <h2>Novo Objetivo</h2>
-                            <div className={styles.inputs}>
-                                <div className={styles.inputObjetivo}>
-                                    <h3>Objetivo</h3>
-                                    <div className={styles.input}>
-                                        <input
-                                            type="text"
-                                            id="nomeObjetivo"
-                                            value={nomeObjetivo}
-                                            onChange={(e) => setNomeObjetivo(e.target.value)}
-                                            placeholder="Digite o nome do objetivo..."
-                                        />
-                                        <Image 
-                                            className={styles.lapis}
-                                            src="/iconeLapisCinza.svg"
-                                            alt="Icone Lapis"
-                                            width={15}
-                                            height={15}
-                                        />
-                                    </div>
-                                    </div>
-                                <div className={styles.inputDescrição}>
-                                    <h3>Descrição</h3>
-                                    <div className={styles.input}>
-                                        <input 
-                                            type="text"
-                                            id="descricaoObjetivo"
-                                            value={descricaoObjetivo}
-                                            onChange={(e) => setDescricaoObjetivo(e.target.value)}
-                                            placeholder="Escreva detalhes sobre o seu objetivo..."
-                                        />
-                                        <Image
-                                            className={styles.lapis} 
-                                            src="/iconeLapisCinza.svg"
-                                            alt="Icone Lapis"
-                                            width={15}
-                                            height={15}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <motion.button
-                                whileHover={{ scale: 1.1 }} 
-                                whileTap={{ scale: 0.8 }}
-                                onClick={handleRegistrarObjetivo}
-                            >
-                                {carregando && <ClipLoader size={10} color="#0B0E31" />}
-                                <span 
-                                    style={{ 
-                                        marginLeft: carregando ? '8px' : '0'
-                                    }}
-                                ></span>
-                                Concluído
-                                <Image 
-                                    className={styles.concluido}
-                                    src="/checkIcon.svg"
-                                    alt="Icone Check"
-                                    width={23}
-                                    height={18}
-                                />
-                            </motion.button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+        </div>
+    );
+
+    // if (!isMobile) {
+    //     return (
+    //         <div className={styles.overlay}>
+    //             <div className={styles.containerPopUp}>
+    //                 <motion.button
+    //                     whileHover={{ scale: 1.1 }} 
+    //                     whileTap={{ scale: 0.8 }}
+    //                     className={styles.botaoVoltar} 
+    //                     onClick={() => window.location.reload()}
+    //                 >
+    //                     <strong>X</strong>
+    //                 </motion.button>
+    //                 <div className={styles.conteudo}>
+    //                     <Image 
+    //                         src="/iconeObjetivo-NovoObjetivo.svg"
+    //                         alt="Icone Objetivo"
+    //                         width={72}
+    //                         height={72}
+    //                         className={styles.icone}
+    //                     />
+    //                     <h2>Novo Objetivo</h2>
+    //                     <div className={styles.inputs}>
+    //                         <div className={styles.inputObjetivo}>
+    //                             <h3>Objetivo</h3>
+    //                             <div className={styles.input}>
+    //                                 <input
+    //                                     type="text"
+    //                                     id="nomeObjetivo"
+    //                                     value={nomeObjetivo}
+    //                                     onChange={(e) => setNomeObjetivo(e.target.value)}
+    //                                     placeholder="Digite o nome do objetivo..."
+    //                                 />
+    //                                 <Image 
+    //                                     className={styles.lapis}
+    //                                     src="/iconeLapisCinza.svg"
+    //                                     alt="Icone Lapis"
+    //                                     width={15}
+    //                                     height={15}
+    //                                 />
+    //                             </div>
+    //                             </div>
+    //                         <div className={styles.inputDescrição}>
+    //                             <h3>Descrição</h3>
+    //                             <div className={styles.input}>
+    //                                 <input 
+    //                                     type="text"
+    //                                     id="descricaoObjetivo"
+    //                                     value={descricaoObjetivo}
+    //                                     onChange={(e) => setDescricaoObjetivo(e.target.value)}
+    //                                     placeholder="Escreva detalhes sobre o seu objetivo..."
+    //                                 />
+    //                                 <Image
+    //                                     className={styles.lapis} 
+    //                                     src="/iconeLapisCinza.svg"
+    //                                     alt="Icone Lapis"
+    //                                     width={15}
+    //                                     height={15}
+    //                                 />
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                     <motion.button
+    //                         whileHover={{ scale: 1.1 }} 
+    //                         whileTap={{ scale: 0.8 }}
+    //                         onClick={handleRegistrarObjetivo}
+    //                     >
+    //                         {carregando && <ClipLoader size={10} color="#0B0E31" />}
+    //                         <span 
+    //                             style={{ 
+    //                                 marginLeft: carregando ? '8px' : '0'
+    //                             }}
+    //                         ></span>
+    //                         Concluído
+    //                         <Image 
+    //                             className={styles.concluido}
+    //                             src="/checkIcon.svg"
+    //                             alt="Icone Check"
+    //                             width={23}
+    //                             height={18}
+    //                         />
+    //                     </motion.button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     );
+    // } else {
+    // }
 
 }
