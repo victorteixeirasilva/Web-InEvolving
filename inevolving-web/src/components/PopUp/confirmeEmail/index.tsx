033b613c805d5680fe287b2f2a7b4cc5417dffd6
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-29 16:10:56 -0300

    Refatorando Componente ConfirmeEmail

--- inevolving-web/src/components/PopUp/confirmeEmail/index.tsx
+++ inevolving-web/src/components/PopUp/confirmeEmail/index.tsx
@@ -11,9 +11,8 @@ export default function ConfirmeEmail( { voltar }: { voltar:() => void } ) {
     }, []);
 
 
-    if (!isMobile) {
-        return (
-            <>
+    return (
+        <div className={isMobile ? styles.mob : ''}>
             <div className={styles.overlay}>
                 <div className={styles.containerPopUp}>
                     <div className={styles.botoesTopo}>
@@ -36,34 +35,37 @@ export default function ConfirmeEmail( { voltar }: { voltar:() => void } ) {
                     </div>
                 </div>
             </div>
-            </>
-        );
-    } else {
-        return (
-            <div className={styles.mob}>
-                <div className={styles.overlay}>
-                    <div className={styles.containerPopUp}>
-                        <div className={styles.botoesTopo}>
-                            <motion.button
-                                whileHover={{ scale: 1.1 }} 
-                                whileTap={{ scale: 0.8 }}
-                                className={styles.botaoVoltar} 
-                                onClick={voltar}
-                            >
-                                <strong>X</strong>
-                            </motion.button>
-                        </div>
-                        <div className={styles.conteudo}>
-                            <h2>E-mail, não confirmado!</h2>
-                            <div className={styles.inputs}>
-                                <div className={styles.inputObjetivo}>
-                                    <h3>Confirme o seu e-mail para fazer login!</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+        </div>
+    );
+
+    // if (!isMobile) {
+    //     return (
+    //         <>
+    //         <div className={styles.overlay}>
+    //             <div className={styles.containerPopUp}>
+    //                 <div className={styles.botoesTopo}>
+    //                     <motion.button
+    //                         whileHover={{ scale: 1.1 }} 
+    //                         whileTap={{ scale: 0.8 }}
+    //                         className={styles.botaoVoltar} 
+    //                         onClick={voltar}
+    //                     >
+    //                         <strong>X</strong>
+    //                     </motion.button>
+    //                 </div>
+    //                 <div className={styles.conteudo}>
+    //                     <h2>E-mail, não confirmado!</h2>
+    //                     <div className={styles.inputs}>
+    //                         <div className={styles.inputObjetivo}>
+    //                             <h3>Confirme o seu e-mail para fazer login!</h3>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         </>
+    //     );
+    // } else {
+
+    // }
 }
