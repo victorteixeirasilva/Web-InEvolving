1440cad59f585284f36fe6cb0ed01d35de9c200c
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-29 16:07:45 -0300

    Refatorando Componente AdicionarNovoObjetivoOuCategoria

--- inevolving-web/src/components/PopUp/adicionarNovoObjetivoOuCategoria/index.tsx
+++ inevolving-web/src/components/PopUp/adicionarNovoObjetivoOuCategoria/index.tsx
@@ -16,9 +16,8 @@ export default function AdicionarNovoObjetivoOuCategoria() {
     const [popUpNovoObjetivo, setPopUpNovoObjetivo] = useState(false);
     const [popUpNovaCategoria, setPopUpNovaCategoria] = useState(false);
 
-    if (!isMobile) {
-        return (
-            <>
+    return (
+        <div className={isMobile ? styles.mob : ''}>
             <div className={styles.overlay}>
                 <div className={styles.containerPopUp}>
                     <motion.button
@@ -83,76 +82,79 @@ export default function AdicionarNovoObjetivoOuCategoria() {
             {popUpNovaCategoria && (
                 <AdicionarNovaCategoria />
             )}
-            </>
-        );
-    } else {
-        return (
-            <div className={styles.mob}>
-                <div className={styles.overlay}>
-                    <div className={styles.containerPopUp}>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }} 
-                            whileTap={{ scale: 0.8 }}
-                            className={styles.botaoVoltar} 
-                            onClick={() => window.location.reload()}
-                            >
-                            <strong style={{color: '#0B0E31'}}>X</strong>
-                        </motion.button>
-                        <div className={styles.conteudo}>
-                            <Image 
-                                src="/iconePlus.svg"
-                                alt="Icone +"
-                                width={72}
-                                height={72}
-                                className={styles.icone}
-                                />
-                            <h2>Adicionar novo</h2>
-                            <div className={styles.botoes}>
-                                <motion.button
-                                    whileHover={{ scale: 1.1 }} 
-                                    whileTap={{ scale: 0.8 }}
-                                    onClick={() => setPopUpNovoObjetivo(true)}
-                                    style={{color: '#0B0E31'}}
-                                >
-                                    <div className={styles.containerBotoes}>
-                                        Objetivo
-                                        <Image 
-                                            src="/iconeSetaDireitaAzul.svg"
-                                            alt="Icone de Seta"
-                                            width={10}
-                                            height={18}
-                                            className={styles.iconeBotao}
-                                            />
-                                    </div>
-                                </motion.button>
-                                <motion.button
-                                    whileHover={{ scale: 1.1 }} 
-                                    whileTap={{ scale: 0.8 }}
-                                    onClick={() => setPopUpNovaCategoria(true)}
-                                    style={{color: '#0B0E31'}}
-                                >
-                                    <div className={styles.containerBotoes}>
-                                        Categoria
-                                        <Image 
-                                            src="/iconeSetaDireitaAzul.svg"
-                                            alt="Icone de Seta"
-                                            width={10}
-                                            height={18}
-                                            className={styles.iconeBotao}
-                                            />
-                                    </div>
-                                </motion.button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {popUpNovoObjetivo && (
-                    <AdicionarNovoObjetivo />
-                )}
-                {popUpNovaCategoria && (
-                    <AdicionarNovaCategoria />
-                )}
-            </div>
-        );
-    }
+        </div>
+    );
+
+
+    // if (!isMobile) {
+    //     return (
+    //         <>
+    //         <div className={styles.overlay}>
+    //             <div className={styles.containerPopUp}>
+    //                 <motion.button
+    //                     whileHover={{ scale: 1.1 }} 
+    //                     whileTap={{ scale: 0.8 }}
+    //                     className={styles.botaoVoltar} 
+    //                     onClick={() => window.location.reload()}
+    //                     >
+    //                     <strong style={{color: '#0B0E31'}}>X</strong>
+    //                 </motion.button>
+    //                 <div className={styles.conteudo}>
+    //                     <Image 
+    //                         src="/iconePlus.svg"
+    //                         alt="Icone +"
+    //                         width={72}
+    //                         height={72}
+    //                         className={styles.icone}
+    //                         />
+    //                     <h2>Adicionar novo</h2>
+    //                     <div className={styles.botoes}>
+    //                         <motion.button
+    //                             whileHover={{ scale: 1.1 }} 
+    //                             whileTap={{ scale: 0.8 }}
+    //                             onClick={() => setPopUpNovoObjetivo(true)}
+    //                             style={{color: '#0B0E31'}}
+    //                         >
+    //                             <div className={styles.containerBotoes}>
+    //                                 Objetivo
+    //                                 <Image 
+    //                                     src="/iconeSetaDireitaAzul.svg"
+    //                                     alt="Icone de Seta"
+    //                                     width={10}
+    //                                     height={18}
+    //                                     className={styles.iconeBotao}
+    //                                     />
+    //                             </div>
+    //                         </motion.button>
+    //                         <motion.button
+    //                             whileHover={{ scale: 1.1 }} 
+    //                             whileTap={{ scale: 0.8 }}
+    //                             onClick={() => setPopUpNovaCategoria(true)}
+    //                             style={{color: '#0B0E31'}}
+    //                         >
+    //                             <div className={styles.containerBotoes}>
+    //                                 Categoria
+    //                                 <Image 
+    //                                     src="/iconeSetaDireitaAzul.svg"
+    //                                     alt="Icone de Seta"
+    //                                     width={10}
+    //                                     height={18}
+    //                                     className={styles.iconeBotao}
+    //                                     />
+    //                             </div>
+    //                         </motion.button>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         {popUpNovoObjetivo && (
+    //             <AdicionarNovoObjetivo />
+    //         )}
+    //         {popUpNovaCategoria && (
+    //             <AdicionarNovaCategoria />
+    //         )}
+    //         </>
+    //     );
+    // } else {
+    // }
 }
