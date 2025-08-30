4299812815f645cac55271241610ec7715a2c920
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-08-30 12:08:27 -0300

    Refatorando Componente EditarSonho

--- inevolving-web/src/components/PopUp/editarSonho/index.tsx
+++ inevolving-web/src/components/PopUp/editarSonho/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { ClipLoader } from 'react-spinners';
 import { useRouter } from "next/navigation";
 import { Sonho } from "@/components/interfaces/Sonho";
+import { linkApi } from "@/app/page";
 
 export default function EditarSonho( { sonho }: { sonho: Sonho } ) {
     const [isMobile, setIsMobile] = useState(false);
@@ -23,7 +24,7 @@ export default function EditarSonho( { sonho }: { sonho: Sonho } ) {
         setCarregando(true);
 
         const response = await fetch(
-            'https://api.inevolving.inovasoft.tech/auth/api/motivation/dreams/'+sonho.id, 
+            linkApi+'/auth/api/motivation/dreams/'+sonho.id, 
             {
                 method: 'DELETE',
                 headers: {
@@ -54,7 +55,7 @@ export default function EditarSonho( { sonho }: { sonho: Sonho } ) {
             setCarregando(true);
 
             const response = await fetch(
-                    'https://api.inevolving.inovasoft.tech/auth/api/motivation/dreams', 
+                linkApi+'/auth/api/motivation/dreams', 
                 {
                         method: 'PATCH',
                         headers: {
@@ -86,8 +87,8 @@ export default function EditarSonho( { sonho }: { sonho: Sonho } ) {
             window.location.reload();
     }
 
-    if (!isMobile) {
-        return (
+    return (
+        <div className={isMobile ? styles.mob : ''}>
             <div className={styles.overlay}>
                 <div className={styles.containerPopUp}>
                     <div className={styles.botoesTopo}>
@@ -103,10 +104,7 @@ export default function EditarSonho( { sonho }: { sonho: Sonho } ) {
                             whileHover={{ scale: 1.2 }} 
                             whileTap={{ scale: 0.8 }}
                             className={styles.lixeira}
-                            onClick={() => {
-                                    if (confirm('Tem certeza que deseja excluir este sonho?')) {
-                                        deletarSonho();
-                                    }}}
+                            onClick={() => {deletarSonho();}}
                         >
                             <Image 
                                 src="/lixeiraIcon.svg"
@@ -220,141 +218,145 @@ export default function EditarSonho( { sonho }: { sonho: Sonho } ) {
                     </div>
                 </div>
             </div>
-        );
-    } else {
-        return (
-            <div className={styles.mob}>
-                <div className={styles.overlay}>
-                    <div className={styles.containerPopUp}>
-                        <div className={styles.botoesTopo}>
-                            <motion.button
-                                whileHover={{ scale: 1.1 }} 
-                                whileTap={{ scale: 0.8 }}
-                                className={styles.botaoVoltar} 
-                                onClick={() => window.location.reload()}
-                            >
-                                <strong>X</strong>
-                            </motion.button>
-                            <motion.button
-                                whileHover={{ scale: 1.2 }} 
-                                whileTap={{ scale: 0.8 }}
-                                className={styles.lixeira}
-                                onClick={() => {deletarSonho();}}
-                            >
-                                <Image 
-                                    src="/lixeiraIcon.svg"
-                                    alt="Icone Lixeira"
-                                    width={27}
-                                    height={29}
-                                />
-                            </motion.button>
-                        </div>
-                        <div className={styles.conteudo}>
-                            <Image 
-                                src="/iconeObjetivo-NovoObjetivo.svg"
-                                alt="Icone Novo Sonho"
-                                width={72}
-                                height={72}
-                                className={styles.icone}
-                            />
-                            <h2>Editar Sonho</h2>
-                            <div className={styles.inputs}>
-                                <div className={styles.inputObjetivo}>
-                                    <h3>Nome do Sonho</h3>
-                                    <div className={styles.input}>
-                                        <input
-                                            type="text"
-                                            id="nomeSonho"
-                                            value={nomeSonho}
-                                            onChange={(e) => setNomeSonho(e.target.value)}
-                                            placeholder={sonho ? sonho.name : "Digite o nome do sonho..."}
-                                        />
-                                        <Image 
-                                            className={styles.lapis}
-                                            src="/iconeLapisCinza.svg"
-                                            alt="Icone Lapis"
-                                            width={15}
-                                            height={15}
-                                            />
-                                    </div>
-                                    </div>
-                                <div className={styles.inputDescrição}>
-                                    <h3>Descrição do Sonho</h3>
-                                    <div className={styles.input}>
-                                        <input 
-                                            type="text"
-                                            id="descricaoSonho"
-                                            value={descricaoSonho}
-                                            onChange={(e) => setDescricaoSonho(e.target.value)}
-                                            placeholder={sonho ? sonho.description : "Digite os detalhe do seu sonho..."}
-                                        />
-                                        <Image
-                                            className={styles.lapis} 
-                                            src="/iconeLapisCinza.svg"
-                                            alt="Icone Lapis"
-                                            width={15}
-                                            height={15}
-                                            />
-                                    </div>
-                                </div>
-                                <div className={styles.inputDescrição}>
-                                    <h3>Url da Imagem</h3>
-                                    <div className={styles.input}>
-                                        <input 
-                                            type="text"
-                                            id="coverImage"
-                                            value={coverImage}
-                                            onChange={(e) => setCoverImage(e.target.value)}
-                                            placeholder={sonho ? sonho.urlImage : "Escreva a url da imagem..."}
-                                        />
-                                        <Image
-                                            className={styles.lapis} 
-                                            src="/iconeLapisCinza.svg"
-                                            alt="Icone Lapis"
-                                            width={15}
-                                            height={15}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <motion.button
-                                whileHover={{ scale: 1.05 }} 
-                                whileTap={{ scale: 0.8 }}
-                                style={ 
-                                        nomeSonho === sonho.name && 
-                                        descricaoSonho === sonho.description &&
-                                        coverImage === sonho.urlImage ? 
-                                        { backgroundColor: '#E0E0E0', cursor: 'not-allowed' } : 
-                                        {}
-                                }
-                                onClick={() => 
-                                        nomeSonho === sonho.name && 
-                                        descricaoSonho === sonho.description &&
-                                        coverImage === sonho.urlImage ? 
-                                        undefined :
-                                        atualizarSonho()
-                                }
-                            >
-                                {carregando && <ClipLoader size={10} color="#0B0E31" />}
-                                <span 
-                                    style={{ 
-                                        marginLeft: carregando ? '8px' : '0'
-                                    }}
-                                ></span>
-                                Salvar
-                                <Image 
-                                    className={styles.concluido}
-                                    src="/checkIcon.svg"
-                                    alt="Icone Check"
-                                    width={23}
-                                    height={18}
-                                />
-                            </motion.button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+        </div>
+    );
+
+    // if (!isMobile) {
+    //     return (
+    //         <div className={styles.overlay}>
+    //             <div className={styles.containerPopUp}>
+    //                 <div className={styles.botoesTopo}>
+    //                     <motion.button
+    //                         whileHover={{ scale: 1.1 }} 
+    //                         whileTap={{ scale: 0.8 }}
+    //                         className={styles.botaoVoltar} 
+    //                         onClick={() => window.location.reload()}
+    //                     >
+    //                         <strong>X</strong>
+    //                     </motion.button>
+    //                     <motion.button
+    //                         whileHover={{ scale: 1.2 }} 
+    //                         whileTap={{ scale: 0.8 }}
+    //                         className={styles.lixeira}
+    //                         onClick={() => {
+    //                                 if (confirm('Tem certeza que deseja excluir este sonho?')) {
+    //                                     deletarSonho();
+    //                                 }}}
+    //                     >
+    //                         <Image 
+    //                             src="/lixeiraIcon.svg"
+    //                             alt="Icone Lixeira"
+    //                             width={27}
+    //                             height={29}
+    //                         />
+    //                     </motion.button>
+    //                 </div>
+    //                 <div className={styles.conteudo}>
+    //                     <Image 
+    //                         src="/iconeObjetivo-NovoObjetivo.svg"
+    //                         alt="Icone Novo Sonho"
+    //                         width={72}
+    //                         height={72}
+    //                         className={styles.icone}
+    //                     />
+    //                     <h2>Editar Sonho</h2>
+    //                     <div className={styles.inputs}>
+    //                         <div className={styles.inputObjetivo}>
+    //                             <h3>Nome do Sonho</h3>
+    //                             <div className={styles.input}>
+    //                                 <input
+    //                                     type="text"
+    //                                     id="nomeSonho"
+    //                                     value={nomeSonho}
+    //                                     onChange={(e) => setNomeSonho(e.target.value)}
+    //                                     placeholder={sonho ? sonho.name : "Digite o nome do sonho..."}
+    //                                 />
+    //                                 <Image 
+    //                                     className={styles.lapis}
+    //                                     src="/iconeLapisCinza.svg"
+    //                                     alt="Icone Lapis"
+    //                                     width={15}
+    //                                     height={15}
+    //                                     />
+    //                             </div>
+    //                             </div>
+    //                         <div className={styles.inputDescrição}>
+    //                             <h3>Descrição do Sonho</h3>
+    //                             <div className={styles.input}>
+    //                                 <input 
+    //                                     type="text"
+    //                                     id="descricaoSonho"
+    //                                     value={descricaoSonho}
+    //                                     onChange={(e) => setDescricaoSonho(e.target.value)}
+    //                                     placeholder={sonho ? sonho.description : "Digite os detalhe do seu sonho..."}
+    //                                 />
+    //                                 <Image
+    //                                     className={styles.lapis} 
+    //                                     src="/iconeLapisCinza.svg"
+    //                                     alt="Icone Lapis"
+    //                                     width={15}
+    //                                     height={15}
+    //                                     />
+    //                             </div>
+    //                         </div>
+    //                         <div className={styles.inputDescrição}>
+    //                             <h3>Url da Imagem</h3>
+    //                             <div className={styles.input}>
+    //                                 <input 
+    //                                     type="text"
+    //                                     id="coverImage"
+    //                                     value={coverImage}
+    //                                     onChange={(e) => setCoverImage(e.target.value)}
+    //                                     placeholder={sonho ? sonho.urlImage : "Escreva a url da imagem..."}
+    //                                 />
+    //                                 <Image
+    //                                     className={styles.lapis} 
+    //                                     src="/iconeLapisCinza.svg"
+    //                                     alt="Icone Lapis"
+    //                                     width={15}
+    //                                     height={15}
+    //                                 />
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                     <motion.button
+    //                         whileHover={{ scale: 1.05 }} 
+    //                         whileTap={{ scale: 0.8 }}
+    //                         style={ 
+    //                                 nomeSonho === sonho.name && 
+    //                                 descricaoSonho === sonho.description &&
+    //                                 coverImage === sonho.urlImage ? 
+    //                                 { backgroundColor: '#E0E0E0', cursor: 'not-allowed' } : 
+    //                                 {}
+    //                         }
+    //                         onClick={() => 
+    //                                 nomeSonho === sonho.name && 
+    //                                 descricaoSonho === sonho.description &&
+    //                                 coverImage === sonho.urlImage ? 
+    //                                 undefined :
+    //                                 atualizarSonho()
+    //                         }
+    //                     >
+    //                         {carregando && <ClipLoader size={10} color="#0B0E31" />}
+    //                         <span 
+    //                             style={{ 
+    //                                 marginLeft: carregando ? '8px' : '0'
+    //                             }}
+    //                         ></span>
+    //                         Salvar
+    //                         <Image 
+    //                             className={styles.concluido}
+    //                             src="/checkIcon.svg"
+    //                             alt="Icone Check"
+    //                             width={23}
+    //                             height={18}
+    //                         />
+    //                     </motion.button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     );
+    // } else {
+    // }
 
 }
