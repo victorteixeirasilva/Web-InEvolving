55787a31ba1c40ff48745a187079dc2f02d68c66
victorteixeriasilva <victorteixeira2308@gmail.com>
2026-02-26 18:13:20 -0300

    d768ac31-b8b5-4cd6-aff6-431ef7e24f8c - Melhorando UI/UX de alteração do tipo de Menu

--- inevolving-web/src/app/ajustes/page.tsx
+++ inevolving-web/src/app/ajustes/page.tsx
@@ -6,11 +6,13 @@ import Menu from "@/components/Menu";
 import styles from './page.module.scss';
 import * as motion from "motion/react-client";
 import { useEffect, useState } from "react";
+import AlterarTipoDoMenuPopUp from "@/components/PopUp/PopUpDeAjustes/AlterarTipoDoMenuPopUp";
 
 export default function Page( ) {
 
     const [isMobile, setIsMobile] = useState(false);
-    const [tipoMenuDesk, setTipoMenuDesk] = useState<number | undefined>(undefined);
+    const [tipoMenuDesk, setTipoMenuDesk] = useState<number | undefined>();
+    const [alterarTipoDoMenuPopUp, setAlterarTipoDoMenuPopUp] = useState<boolean>(false);
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -26,53 +28,59 @@ export default function Page( ) {
     }, []);
 
     return (
-        <motion.div className={isMobile ? styles.mob : tipoMenuDesk === 2 ? styles.containerTipoMenu2 : ''}>
-            <Menu />
-            <motion.div
-                initial={{ opacity: 0, scale: 0.97 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                    duration: 0.7,
-                    scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
-                }} 
-                className={styles.container}
-            >
-                <div className={styles.tituloContainer}>
-                    <h1>Ajustes</h1>
-                    <p>Altere suas informações de login</p>
-                </div>
-                <motion.div className={styles.containerConteudo}>
-                    <button 
-                        onClick={() => {
-                            const novoTipo = tipoMenuDesk === 1 ? 2 : 1;
-                            localStorage.setItem('tipoMenuDesk', novoTipo.toString());
-                            window.location.reload();
-                        }}
-                    >
-                        Alterar Tipo do Menu
-                    </button>
-                    <button>
-                        {/* TODO - Desenvolver Tela de Pop Up */}
-                        Alterar Informações do Usuário
-                    </button>
-                    <button>
-                        {/* TODO - Desenvolver Tela de Pop Up */}
-                        Alterar Senha
-                    </button>
-                    <button>
-                        {/* TODO - Desenvolver Tela de Pop Up */}
-                        Alterar Tema
-                    </button>
-                    <button>
-                        {/* TODO - Desenvolver Tela de Pop Up */}
-                        Ver Lista de Amigos
-                    </button>
-                    <button>
-                        {/* TODO - Desenvolver Tela de Pop Up */}
-                        Alterar Data da Renovação
-                    </button>
+        <>
+            <motion.div className={isMobile ? styles.mob : tipoMenuDesk === 2 ? styles.containerTipoMenu2 : ''}>
+                <Menu />
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.97 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                        duration: 0.7,
+                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+                    }} 
+                    className={styles.container}
+                >
+                    <div className={styles.tituloContainer}>
+                        <h1>Ajustes</h1>
+                        <p>Altere suas informações de login</p>
+                    </div>
+                    <motion.div className={styles.containerConteudo}>
+                        <button 
+                            // onClick={() => {
+                            //     const novoTipo = tipoMenuDesk === 1 ? 2 : 1;
+                            //     localStorage.setItem('tipoMenuDesk', novoTipo.toString());
+                            //     window.location.reload();
+                            // }}
+                            onClick={() => setAlterarTipoDoMenuPopUp(true)}
+                        >
+                            Alterar Tipo do Menu
+                        </button>
+                        <button>
+                            {/* TODO - Desenvolver Tela de Pop Up */}
+                            Alterar Informações do Usuário
+                        </button>
+                        <button>
+                            {/* TODO - Desenvolver Tela de Pop Up */}
+                            Alterar Senha
+                        </button>
+                        <button>
+                            {/* TODO - Desenvolver Tela de Pop Up */}
+                            Alterar Tema
+                        </button>
+                        <button>
+                            {/* TODO - Desenvolver Tela de Pop Up */}
+                            Ver Lista de Amigos
+                        </button>
+                        <button>
+                            {/* TODO - Desenvolver Tela de Pop Up */}
+                            Alterar Data da Renovação
+                        </button>
+                    </motion.div>
                 </motion.div>
             </motion.div>
-        </motion.div>
+            {alterarTipoDoMenuPopUp && (
+                <AlterarTipoDoMenuPopUp />
+            )}
+        </>
     );
 }
---
+++ inevolving-web/src/components/PopUp/PopUpDeAjustes/AlterarTipoDoMenuPopUp/index.tsx
@@ -0,0 +1,123 @@
+// import Image from "next/image";
+import styles from "./AlterarTipoDoMenuPopUp.module.scss";
+import * as motion from "motion/react-client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+// import { ClipLoader } from 'react-spinners';
+// import VerListaDeTarefas from "../../VerListaDeTarefas";
+
+export default function AlterarTipoDoMenuPopUp() {
+    const [isMobile, setIsMobile] = useState(false);
+    
+    const [tipoMenuDesk, setTipoMenuDesk] = useState<number>(0);
+
+    useEffect(() => {
+        const largura = window.innerWidth;
+        setIsMobile(largura <= 1024);
+        setTipoMenuDesk(
+            localStorage.getItem('tipoMenuDesk') ? 
+            parseInt(localStorage.getItem('tipoMenuDesk') as string) : 1
+        );
+    }, []);
+
+    // const [carregando, setCarregando] = useState(false);
+    // const [jwtToken, setJwtToken] = useState('');
+
+    // useEffect(() => {
+    //     const token = localStorage.getItem('token');
+    //     setJwtToken(token ?? '');
+    // }, []);
+    
+
+    return (
+        <div className={isMobile ? styles.mob : ''}>
+            <div className={styles.overlay}>
+                <div className={styles.containerPopUp}>
+                    <div className={styles.botoesTopo}>
+                        <motion.button
+                            whileHover={{ scale: 1.1 }} 
+                            whileTap={{ scale: 0.8 }}
+                            className={styles.botaoVoltar} 
+                            onClick={() => window.location.reload()}
+                        >
+                            <strong>X</strong>
+                        </motion.button>
+                    </div>
+                    <div className={styles.conteudo}>
+                        <h2>Alterar Tipo Do Menu</h2>
+                        <div className={styles.inputs}>
+                            <h3>Selecione o Menu que deixe mais agradavel pra você!</h3>
+                            <motion.div 
+                                className={styles.inputDescrição}
+                                whileHover={{ scale: 1.02 }} 
+                                whileTap={{ scale: 0.95 }}
+                                onClick={() => {
+                                    const novoTipo = 1;
+                                    localStorage.setItem('tipoMenuDesk', novoTipo.toString());
+                                    window.location.reload();
+                                }} 
+                            >
+                                <div 
+                                    className={styles.inputStatus}
+                                    style={
+                                        tipoMenuDesk === 1 ? { 
+                                            color: '#0b0e31',
+                                            border: 'solid 3px #0b0e31' 
+                                        } : {}
+                                    }
+                                >
+                                    <p style={tipoMenuDesk === 1 ? { color: '#0b0e31' } : {}}>
+                                        Menu Tipo 1
+                                    </p>
+                                    <Image
+                                        src="/tipoMenu01.png"
+                                        alt='Imagem de Exemplo do Menu tipo 01'
+                                        width={300}
+                                        height={300}
+                                        style={{marginBottom: "20px"}}
+                                    />
+                                </div>
+                            </motion.div>
+                            <motion.div 
+                                style={{marginTop: "20px"}}
+                                className={styles.inputDescrição}
+                                whileHover={{ scale: 1.02 }} 
+                                whileTap={{ scale: 0.95 }}
+                                onClick={() => {
+                                    const novoTipo = 2;
+                                    localStorage.setItem('tipoMenuDesk', novoTipo.toString());
+                                    window.location.reload();
+                                }} 
+                            >
+                                <div 
+                                    className={styles.inputStatus}
+                                    style={
+                                        tipoMenuDesk === 2 ? { 
+                                            color: '#0b0e31',
+                                            border: 'solid 3px #0b0e31' 
+                                        } : {}
+                                    }
+                                >
+                                    <p style={
+                                        tipoMenuDesk === 2 ? { 
+                                            color: '#0b0e31'
+                                        } : {}}
+                                    >
+                                        Menu Tipo 2
+                                    </p>
+                                    <Image
+                                        src="/tipoMenu02.png"
+                                        alt='Imagem de Exemplo do Menu tipo 02'
+                                        width={300}
+                                        height={300}
+                                        style={{marginBottom: "20px"}}
+                                    />
+                                </div>
+                            </motion.div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
