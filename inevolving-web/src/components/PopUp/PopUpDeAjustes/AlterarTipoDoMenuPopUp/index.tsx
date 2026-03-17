90e553c4d0e49bb40171b182c7d1491855dce9cb
victorteixeriasilva <victorteixeira2308@gmail.com>
2026-03-17 16:56:13 -0300

    Refatorando e adiconando tema dark em AlterarTipoDoMenuPopUp

--- inevolving-web/src/components/PopUp/PopUpDeAjustes/AlterarTipoDoMenuPopUp/index.tsx
+++ inevolving-web/src/components/PopUp/PopUpDeAjustes/AlterarTipoDoMenuPopUp/index.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 // import VerListaDeTarefas from "../../VerListaDeTarefas";
 
 export default function AlterarTipoDoMenuPopUp() {
+    const [tema, setTema] = useState<number | undefined>(undefined);
     const [isMobile, setIsMobile] = useState(false);
     
     const [tipoMenuDesk, setTipoMenuDesk] = useState<number>(0);
@@ -17,6 +18,10 @@ export default function AlterarTipoDoMenuPopUp() {
         setTipoMenuDesk(
             localStorage.getItem('tipoMenuDesk') ? 
             parseInt(localStorage.getItem('tipoMenuDesk') as string) : 1
+        );
+        setTema(
+            localStorage.getItem('tema') ?
+            parseInt(localStorage.getItem('tema') as string) : 1
         );
     }, []);
 
@@ -30,90 +35,92 @@ export default function AlterarTipoDoMenuPopUp() {
     
 
     return (
-        <div className={isMobile ? styles.mob : ''}>
-            <div className={styles.overlay}>
-                <div className={styles.containerPopUp}>
-                    <div className={styles.botoesTopo}>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }} 
-                            whileTap={{ scale: 0.8 }}
-                            className={styles.botaoVoltar} 
-                            onClick={() => window.location.reload()}
-                        >
-                            <strong>X</strong>
-                        </motion.button>
-                    </div>
-                    <div className={styles.conteudo}>
-                        <h2>Alterar Tipo Do Menu</h2>
-                        <div className={styles.inputs}>
-                            <h3>Selecione o Menu que deixe mais agradavel pra você!</h3>
-                            <motion.div 
-                                className={styles.inputDescrição}
-                                whileHover={{ scale: 1.02 }} 
-                                whileTap={{ scale: 0.95 }}
-                                onClick={() => {
-                                    const novoTipo = 2;
-                                    localStorage.setItem('tipoMenuDesk', novoTipo.toString());
-                                    window.location.reload();
-                                }} 
+        <div className={tema === 1 ? styles.dark : styles.temaClaro}>
+            <div className={isMobile ? styles.mob : ''}>
+                <div className={styles.overlay}>
+                    <div className={styles.containerPopUp}>
+                        <div className={styles.botoesTopo}>
+                            <motion.button
+                                whileHover={{ scale: 1.1 }} 
+                                whileTap={{ scale: 0.8 }}
+                                className={styles.botaoVoltar} 
+                                onClick={() => window.location.reload()}
                             >
-                                <div 
-                                    className={styles.inputStatus}
-                                    style={
-                                        tipoMenuDesk === 2 ? { 
-                                            color: '#0b0e31',
-                                            border: 'solid 3px #0b0e31' 
-                                        } : {}
-                                    }
+                                <strong>X</strong>
+                            </motion.button>
+                        </div>
+                        <div className={styles.conteudo}>
+                            <h2>Alterar Tipo Do Menu</h2>
+                            <div className={styles.inputs}>
+                                <h3>Selecione o Menu que deixe mais agradavel pra você!</h3>
+                                <motion.div 
+                                    className={styles.inputDescrição}
+                                    whileHover={{ scale: 1.02 }} 
+                                    whileTap={{ scale: 0.95 }}
+                                    onClick={() => {
+                                        const novoTipo = 2;
+                                        localStorage.setItem('tipoMenuDesk', novoTipo.toString());
+                                        window.location.reload();
+                                    }} 
                                 >
-                                    <p style={tipoMenuDesk === 2 ? { color: '#0b0e31' } : {}}>
-                                        Menu Tipo 1
-                                    </p>
-                                    <Image
-                                        src="/tipoMenu01.png"
-                                        alt='Imagem de Exemplo do Menu tipo 01'
-                                        width={isMobile ? 200 : 300}
-                                        height={isMobile ? 200 : 300}
-                                        style={{marginBottom: "20px"}}
-                                    />
-                                </div>
-                            </motion.div>
-                            <motion.div 
-                                style={{marginTop: "20px"}}
-                                className={styles.inputDescrição}
-                                whileHover={{ scale: 1.02 }} 
-                                whileTap={{ scale: 0.95 }}
-                                onClick={() => {
-                                    const novoTipo = 1;
-                                    localStorage.setItem('tipoMenuDesk', novoTipo.toString());
-                                    window.location.reload();
-                                }} 
-                            >
-                                <div 
-                                    className={styles.inputStatus}
-                                    style={
-                                        tipoMenuDesk === 1 ? { 
-                                            color: '#0b0e31',
-                                            border: 'solid 3px #0b0e31' 
-                                        } : {}
-                                    }
-                                >
-                                    <p style={
-                                        tipoMenuDesk === 1 ? { 
-                                            color: '#0b0e31'
-                                        } : {}}
+                                    <div 
+                                        className={styles.inputStatus}
+                                        style={
+                                            tipoMenuDesk === 2 ? { 
+                                                color: '#0b0e31',
+                                                border: 'solid 3px #0b0e31' 
+                                            } : {}
+                                        }
                                     >
-                                        Menu Tipo 2
-                                    </p>
-                                    <Image
-                                        src="/tipoMenu02.png"
-                                        alt='Imagem de Exemplo do Menu tipo 02'
-                                        width={isMobile ? 200 : 300}
-                                        height={isMobile ? 200 : 300}
-                                        style={{marginBottom: "20px"}}
-                                    />
-                                </div>
-                            </motion.div>
+                                        <p style={tipoMenuDesk === 2 ? { color: '#0b0e31' } : {}}>
+                                            Menu Tipo 1
+                                        </p>
+                                        <Image
+                                            src="/tipoMenu01.png"
+                                            alt='Imagem de Exemplo do Menu tipo 01'
+                                            width={isMobile ? 200 : 300}
+                                            height={isMobile ? 200 : 300}
+                                            style={{marginBottom: "20px"}}
+                                        />
+                                    </div>
+                                </motion.div>
+                                <motion.div 
+                                    style={{marginTop: "20px"}}
+                                    className={styles.inputDescrição}
+                                    whileHover={{ scale: 1.02 }} 
+                                    whileTap={{ scale: 0.95 }}
+                                    onClick={() => {
+                                        const novoTipo = 1;
+                                        localStorage.setItem('tipoMenuDesk', novoTipo.toString());
+                                        window.location.reload();
+                                    }} 
+                                >
+                                    <div 
+                                        className={styles.inputStatus}
+                                        style={
+                                            tipoMenuDesk === 1 ? { 
+                                                color: '#0b0e31',
+                                                border: 'solid 3px #0b0e31' 
+                                            } : {}
+                                        }
+                                    >
+                                        <p style={
+                                            tipoMenuDesk === 1 ? { 
+                                                color: '#0b0e31'
+                                            } : {}}
+                                        >
+                                            Menu Tipo 2
+                                        </p>
+                                        <Image
+                                            src="/tipoMenu02.png"
+                                            alt='Imagem de Exemplo do Menu tipo 02'
+                                            width={isMobile ? 200 : 300}
+                                            height={isMobile ? 200 : 300}
+                                            style={{marginBottom: "20px"}}
+                                        />
+                                    </div>
+                                </motion.div>
+                            </div>
                         </div>
                     </div>
                 </div>
