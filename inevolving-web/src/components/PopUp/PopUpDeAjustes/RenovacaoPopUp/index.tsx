745b1f63eae38c803dc279a7c5b550f4af338282
victorteixeriasilva <victorteixeira2308@gmail.com>
2026-03-17 16:26:02 -0300

    Refatorando e adiconando tema dark em RenovacaoPopUp

--- inevolving-web/src/components/PopUp/PopUpDeAjustes/RenovacaoPopUp/index.tsx
+++ inevolving-web/src/components/PopUp/PopUpDeAjustes/RenovacaoPopUp/index.tsx
@@ -5,45 +5,52 @@ import { useEffect, useState } from "react";
 // import { ClipLoader } from "react-spinners";
 
 export default function RenovacaoPopUp() {
+    const [tema, setTema] = useState<number | undefined>(undefined);
     const [isMobile, setIsMobile] = useState(false);
     const [dataNovaTarefa] = useState<Date | null>(new Date);
     
     useEffect(() => {
         const largura = window.innerWidth;
         setIsMobile(largura <= 1024);
+        setTema(
+            localStorage.getItem('tema') ?
+            parseInt(localStorage.getItem('tema') as string) : 1
+        );
     }, []);
 
 
     return (
-        <div className={isMobile ? styles.mob : ''}>
-            <div className={styles.overlay}>
-                <div className={styles.containerPopUp}>
-                    <div className={styles.botoesTopo}>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }} 
-                            whileTap={{ scale: 0.8 }}
-                            className={styles.botaoVoltar} 
-                            onClick={() => window.location.reload()}
-                        >
-                            <strong>X</strong>
-                        </motion.button>
-                    </div>
-                    <div className={styles.conteudo}>
-                        <h2>Alterar Renovação</h2>
-                        <div>
-                            <div className={styles.containerDataFinal}>
-                                    <p>
-                                        Próxima Renovação do Em 
-                                    </p>
-                                    <div 
-                                        className={styles.inputDataFinal}
-                                        style={{marginTop: '0px'}}
-                                    >
-                                        {dataNovaTarefa && (<strong>{dataNovaTarefa.toLocaleDateString()}</strong>)} 
-                                    </div>
-                            </div>
+        <div className={tema === 1 ? styles.dark : styles.temaClaro}>            
+            <div className={isMobile ? styles.mob : ''}>
+                <div className={styles.overlay}>
+                    <div className={styles.containerPopUp}>
+                        <div className={styles.botoesTopo}>
+                            <motion.button
+                                whileHover={{ scale: 1.1 }} 
+                                whileTap={{ scale: 0.8 }}
+                                className={styles.botaoVoltar} 
+                                onClick={() => window.location.reload()}
+                            >
+                                <strong>X</strong>
+                            </motion.button>
                         </div>
-                        <button>Cancelar Renovação</button>
+                        <div className={styles.conteudo}>
+                            <h2>Alterar Renovação</h2>
+                            <div>
+                                <div className={styles.containerDataFinal}>
+                                        <p>
+                                            Próxima Renovação do Em 
+                                        </p>
+                                        <div 
+                                            className={styles.inputDataFinal}
+                                            style={{marginTop: '0px'}}
+                                        >
+                                            {dataNovaTarefa && (<strong>{dataNovaTarefa.toLocaleDateString()}</strong>)} 
+                                        </div>
+                                </div>
+                            </div>
+                            <button>Cancelar Renovação</button>
+                        </div>
                     </div>
                 </div>
             </div>
