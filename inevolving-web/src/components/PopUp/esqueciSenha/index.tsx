e2d9b0bdccff65ae29ea658820807fb2c8f2506d
victorteixeriasilva <victorteixeira2308@gmail.com>
2026-03-19 16:07:01 -0300

    Adicionando tema dark e refatorando Pop Up de EsqueciSenha

--- inevolving-web/src/components/PopUp/esqueciSenha/index.tsx
+++ inevolving-web/src/components/PopUp/esqueciSenha/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./EditarObjetivo.module.scss";
+import styles from "./EsqueciSenha.module.scss";
 import * as motion from "motion/react-client";
 import { useEffect, useState } from "react";
 import { ClipLoader } from 'react-spinners';
@@ -8,10 +8,15 @@ import { linkApi } from "@/constants";
 export default function EsqueciSenha( { voltar }: { voltar:() => void } ) {
     const [isMobile, setIsMobile] = useState(false);
     const [email, setEmail] = useState("");
+    const [tema, setTema] = useState<number | undefined>(undefined);
     
     useEffect(() => {
         const largura = window.innerWidth;
         setIsMobile(largura <= 1024);
+        setTema(
+            localStorage.getItem('tema') ?
+            parseInt(localStorage.getItem('tema') as string) : 1
+        );
     }, []);
 
 
@@ -45,79 +50,7 @@ export default function EsqueciSenha( { voltar }: { voltar:() => void } ) {
 
     if (!isMobile) {
         return (
-            <>
-            <div className={styles.overlay}>
-                <div className={styles.containerPopUp}>
-                    <div className={styles.botoesTopo}>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }} 
-                            whileTap={{ scale: 0.8 }}
-                            className={styles.botaoVoltar} 
-                            onClick={voltar}
-                        >
-                            <strong>X</strong>
-                        </motion.button>
-                    </div>
-                    <div className={styles.conteudo}>
-                        <h2>Esqueceu a Senha?</h2>
-                        <div className={styles.inputs}>
-                            <div className={styles.inputObjetivo}>
-                                <h3>Informe seu e-mail cadastrado, que enviaremos um link para redefinir a sua senha!</h3>
-                                <div className={styles.input}>
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        placeholder="Digite seu email..."
-                                    />
-                                    <Image 
-                                        className={styles.lapis}
-                                        src="/iconeLapisCinza.svg"
-                                        alt="Icone Lapis"
-                                        width={15}
-                                        height={15}
-                                        />
-                                </div>
-                            </div>
-                        </div>
-                        <motion.button
-                            whileHover={{ scale: 1.05 }} 
-                            whileTap={{ scale: 0.8 }}
-                            style={ 
-                                    email === "" ? 
-                                    { backgroundColor: '#E0E0E0', cursor: 'not-allowed' } : 
-                                    {}
-                            }
-                            onClick={() => 
-                                email === "" ?
-                                undefined :
-                                enviarEmailEsqueciSenha()
-                            }
-                        >
-                            {carregando && <ClipLoader size={10} color="#0B0E31" />}
-                            <span 
-                                style={{ 
-                                    marginLeft: carregando ? '8px' : '0'
-                                }}
-                            ></span>
-                            Enviar
-                            <Image 
-                                className={styles.concluido}
-                                src="/checkIcon.svg"
-                                alt="Icone Check"
-                                width={23}
-                                height={18}
-                            />
-                        </motion.button>
-                    </div>
-                </div>
-            </div>
-            </>
-        );
-    } else {
-        return (
-            <div className={styles.mob}>
+            <div className={tema === 1 ? styles.dark : styles.temaClaro}>
                 <div className={styles.overlay}>
                     <div className={styles.containerPopUp}>
                         <div className={styles.botoesTopo}>
@@ -158,7 +91,7 @@ export default function EsqueciSenha( { voltar }: { voltar:() => void } ) {
                                 whileTap={{ scale: 0.8 }}
                                 style={ 
                                         email === "" ? 
-                                        { backgroundColor: '#E0E0E0', cursor: 'not-allowed' } : 
+                                        { opacity: '0.2', cursor: 'not-allowed' } : 
                                         {}
                                 }
                                 onClick={() => 
@@ -174,7 +107,81 @@ export default function EsqueciSenha( { voltar }: { voltar:() => void } ) {
                                     }}
                                 ></span>
                                 Enviar
+                                <Image 
+                                    className={styles.concluido}
+                                    src="/checkIcon.svg"
+                                    alt="Icone Check"
+                                    width={23}
+                                    height={18}
+                                />
                             </motion.button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className={tema === 1 ? styles.dark : styles.temaClaro}>
+                <div className={styles.mob}>
+                    <div className={styles.overlay}>
+                        <div className={styles.containerPopUp}>
+                            <div className={styles.botoesTopo}>
+                                <motion.button
+                                    whileHover={{ scale: 1.1 }} 
+                                    whileTap={{ scale: 0.8 }}
+                                    className={styles.botaoVoltar} 
+                                    onClick={voltar}
+                                >
+                                    <strong>X</strong>
+                                </motion.button>
+                            </div>
+                            <div className={styles.conteudo}>
+                                <h2>Esqueceu a Senha?</h2>
+                                <div className={styles.inputs}>
+                                    <div className={styles.inputObjetivo}>
+                                        <h3>Informe seu e-mail cadastrado, que enviaremos um link para redefinir a sua senha!</h3>
+                                        <div className={styles.input}>
+                                            <input
+                                                type="text"
+                                                id="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                placeholder="Digite seu email..."
+                                            />
+                                            <Image 
+                                                className={styles.lapis}
+                                                src="/iconeLapisCinza.svg"
+                                                alt="Icone Lapis"
+                                                width={15}
+                                                height={15}
+                                                />
+                                        </div>
+                                    </div>
+                                </div>
+                                <motion.button
+                                    whileHover={{ scale: 1.05 }} 
+                                    whileTap={{ scale: 0.8 }}
+                                    style={ 
+                                            email === "" ? 
+                                            { opacity: '0.2', cursor: 'not-allowed' } : 
+                                            {}
+                                    }
+                                    onClick={() => 
+                                        email === "" ?
+                                        undefined :
+                                        enviarEmailEsqueciSenha()
+                                    }
+                                >
+                                    {carregando && <ClipLoader size={10} color="#0B0E31" />}
+                                    <span 
+                                        style={{ 
+                                            marginLeft: carregando ? '8px' : '0'
+                                        }}
+                                    ></span>
+                                    Enviar
+                                </motion.button>
+                            </div>
                         </div>
                     </div>
                 </div>
